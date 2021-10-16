90742cec401de808e554390f8f56fb017de10ff0
CaronGregoire <gregoirecaron33@gmail.com>
2021-10-16 12:05:09 +0200

    create file for quest 2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8f339ea105c78c1db683117d1349b1d442831c20',
+        'name' => 'greg/composer_2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'greg/composer_2' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8f339ea105c78c1db683117d1349b1d442831c20',
+            'dev_requirement' => false,
+        ),
+    ),
+);
